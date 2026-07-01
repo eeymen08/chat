1bca85c94567fa2a8a4824c97cc391972b421e4d
eeymen08 <ciftcieymen8@gmail.com>
2026-07-01 03:24:47 +0300

    Update server.py

--- server.py
+++ server.py
@@ -3,11 +3,24 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
+socketio = SocketIO(
+    app,
+    cors_allowed_origins="*",
+    async_mode="threading"
+)
 
 @socketio.on("message")
 def handle(msg):
+    print("Mesaj:", msg)
     socketio.emit("message", msg)
+
+@socketio.on("connect")
+def connect():
+    print("Bir kullanıcı bağlandı")
+
+@socketio.on("disconnect")
+def disconnect():
+    print("Bir kullanıcı çıktı")
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=10000)
