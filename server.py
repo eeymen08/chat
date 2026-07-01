6b99668002bbb55cd6ffa0089deda42bd5e2f4cc
eeymen08 <ciftcieymen8@gmail.com>
2026-07-01 03:24:15 +0300

    Update server.py

--- server.py
+++ server.py
@@ -7,17 +7,7 @@ socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
 
 @socketio.on("message")
 def handle(msg):
-    print("Mesaj:", msg)
     socketio.emit("message", msg)
 
-@socketio.on("connect")
-def connect():
-    print("Bir kullanıcı bağlandı")
-
-@socketio.on("disconnect")
-def disconnect():
-    print("Bir kullanıcı çıktı")
-
 if __name__ == "__main__":
-    print("Server çalışıyor http://127.0.0.1:5000")
-    socketio.run(app, host="127.0.0.1", port=5000)
+    socketio.run(app, host="0.0.0.0", port=10000)
